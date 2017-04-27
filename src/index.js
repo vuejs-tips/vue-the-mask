9e404108cc84ad4370e77a813b89e2871e6fd671
Marcos Neves <marcos.neves@gmail.com>
2017-04-27 12:24:34 -0300

    Fix module exported

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 const tokens = require('./tokens')
 const TheMask = require('./TheMask.vue')
 
-const Component = {
-  tokens,
-  TheMask,
-  install: Vue => Vue.component(TheMask.name, TheMask),
-  version: proccess.env.VERSION
-}
+TheMask.tokens = tokens
+TheMask.install = Vue => Vue.component(TheMask.name, TheMask)
+TheMask.version = proccess.env.VERSION
 
-module.exports = Component
+module.exports = TheMask
 
 // Install by default if included from script tag
 if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(Component)
+  window.Vue.use(TheMask)
 }
