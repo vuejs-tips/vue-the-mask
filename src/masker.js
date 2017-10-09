c0da575b98079eaef54451d1150cf53ff9053f0b
Marcello Gesmundo <mgesmundo@yoovant.com>
2017-10-09 16:46:59 +0200

    Avoid error on empty mask using directive

--- src/masker.js
+++ src/masker.js
@@ -2,6 +2,8 @@ import maskit from './maskit'
 import dynamicMask from './dynamic-mask'
 // Facade to maskit/dynamicMask when mask is String or Array
 export default function (value, mask, masked = true, tokens) {
+  // disable on empty mask 
+  if (!mask) { return value }
   return Array.isArray(mask)
          ? dynamicMask(maskit, mask, tokens)(value, mask, masked, tokens)
          : maskit(value, mask, masked, tokens)
