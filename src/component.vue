2d7aba62fc6a5f36bbd11d956e0c1378fce9d329
c01nd01r <stas.er.it@gmail.com>
2017-10-23 20:05:25 +0300

    Add $listeners to component

--- src/component.vue
+++ src/component.vue
@@ -1,5 +1,5 @@
 <template>
-<input type="text" v-mask="config" :value="display" @input="onInput" />
+<input type="text" v-mask="config" :value="display" v-on="$listeners" @input="onInput" />
 </template>
 
 <script>
