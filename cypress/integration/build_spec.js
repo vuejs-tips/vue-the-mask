e113389c7e0f7a44d0785591d5076508d57e3079
Marcos Neves | ubuntu@home <marcos.neves@gmail.com>
2018-05-16 14:12:14 -0300

    e2e tests using cypress

---
+++ cypress/integration/build_spec.js
@@ -0,0 +1,23 @@
+const PATH = '/cypress/build-fixture.html'
+
+describe('dist build test', function() {
+  it('loads VueTheMask at window', function() {
+    const o = { delay: 500 }
+    cy
+      .visit(PATH)
+      .get('#input')
+      .type('0{leftarrow}1', o)
+      .then(input => console.log(input.get(0).selectionEnd))
+    // .should('have.value', '+1 0')
+    // .should('have.value', '+1 02')
+    // .type('3', o)
+    // .should('have.value', '+1 02.3')
+    // .type('{backspace}', o)
+    // .should('have.value', '+1 02.')
+    // .type('1{leftarrow}2{leftarrow}3', o)
+    // .type('56{uparrow}0', o)
+    // .should('have.value', '+1 03.42')
+    // .get('#cursor')
+    // .should('have.text', '7')
+  })
+})
