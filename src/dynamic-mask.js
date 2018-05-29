c490194067160f844e61384981672e86c036c2f0
ThallisPHP <thallisphp@gmail.com>
2018-05-29 11:08:30 -0300

    Ajuste quando a máscara é passada por prop dinâmica
    Quando uma máscara é passada por prop dinâmica e no formato de array um loop era iniciado ao executar a ordenação das máscaras.
    Convertendo o array de máscaras (que possui um observer) para um array normal antes da ordenação resolveu o problema.

--- src/dynamic-mask.js
+++ src/dynamic-mask.js
@@ -1,11 +1,11 @@
 export default function dynamicMask (maskit, masks, tokens) {
-  masks = masks.sort((a, b) => a.length - b.length)
+  let sortedMasks = Array.from(masks).sort((a, b) => a.length - b.length)
   return function (value, mask, masked = true) {
     var i = 0
-    while (i < masks.length) {
-      var currentMask = masks[i]
+    while (i < sortedMasks.length) {
+      var currentMask = sortedMasks[i]
       i++
-      var nextMask = masks[i]
+      var nextMask = sortedMasks[i]
       if (! (nextMask && maskit(value, nextMask, true, tokens).length > currentMask.length) ) {
         return maskit(value, currentMask, masked, tokens)
       }
