ac69a732c4e55fc3960bfdc746bf6d8c0b487c01
Jordi Serra Gonzalez <jserrago@everis.com>
2019-03-13 12:14:06 +0100

    export all files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import tokens from './tokens'
-import mask from './directive'
-import TheMask from './component.vue'
+import tokens from "./tokens";
+import mask from "./directive";
+import TheMask from "./component.vue";
+import dynamicMask from "./dynamic-mask";
+import masker from "./masker";
+import maskit from "./maskit";
 
 function install(Vue) {
-  Vue.component(TheMask.name, TheMask)
-  Vue.directive('mask', mask)
+  Vue.component(TheMask.name, TheMask);
+  Vue.directive("mask", mask);
 }
 
-export default install
-export { TheMask, mask, tokens }
+export default install;
+export { TheMask, mask, tokens, dynamicMask, masker, maskit };
 
 // Install by default if included from script tag
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(install)
+if (typeof window !== "undefined" && window.Vue) {
+  window.Vue.use(install);
 }
