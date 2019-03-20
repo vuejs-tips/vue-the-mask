99830469efa9796a53faf6d83b583a0601dd0561
Joël Poulin <joel.poulin@infocentric.ch>
2019-03-20 16:41:21 +0100

    allow empty mask (in that case, the directive will be ignored)

--- src/masker.js
+++ src/masker.js
@@ -2,6 +2,9 @@ import maskit from './maskit'
 import dynamicMask from './dynamic-mask'
 // Facade to maskit/dynamicMask when mask is String or Array
 export default function (value, mask, masked = true, tokens) {
+  if (!mask) {
+    return value;
+  }
   return Array.isArray(mask)
          ? dynamicMask(maskit, mask, tokens)(value, mask, masked, tokens)
          : maskit(value, mask, masked, tokens)
