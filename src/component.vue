444772094409048dbfe34907e56384460a28eb3b
Christophe Gondouin <14264672+cgoconseils@users.noreply.github.com>
2020-04-02 11:35:34 +0200

    Adding focus and blur event support

--- src/component.vue
+++ src/component.vue
@@ -1,5 +1,5 @@
 <template>
-<input type="text" v-mask="config" :value="display" @input="onInput" />
+<input type="text" v-mask="config" :value="display" @input="onInput" @focus="$emit('focus')" @blur="$emit('blur')" />
 </template>
 
 <script>
